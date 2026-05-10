d56eee5fc5a42d951635e2c41b551346b88df424
kolanusarvajith <kolanusarvajith@gmail.com>
2026-05-10 14:27:54 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> prev(n) , nxt(n);
+
+        vector<int> v; // This acts like monotonic stack
+        // prev[0] = -1;
+
+        // v.push_back(0);
+        for(int i = 0 ; i < n ; i++){
+            if(v.empty() || height[i] > height[v.back()])
+                v.push_back(i);
+            
+            prev[i] = v.back();
+        }
+
+        v.clear();
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(v.empty() || height[i] > height[v.back()])
+                v.push_back(i);
+            
+            nxt[i] = v.back();
+        }
+
+        int ans = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            ans += min(height[nxt[i]] , height[prev[i]]) - height[i];
+        }
+
+        return ans;
+
+    }
+};
